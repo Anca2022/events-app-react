94f4721cb74c3588b0225260cf748640c5dab31c
Anca Matei <ancamatei2012@gmail.com>
2024-08-23 13:29:41 +0300

    EventDetails done for mobile

--- src/components/EventDetailsHeader.js
+++ src/components/EventDetailsHeader.js
@@ -1,11 +1,12 @@
 import {ReactComponent as Back} from '../assets/left.svg';
 import {ReactComponent as External} from '../assets/external-link.svg';
 import {ReactComponent as Favorite} from '../assets/favorite.svg';
+import { Link } from 'react-router-dom';
 export default function EventDetailsHeader() {
     return (
         <header className="event-header">
             <div className='event-header-btns'>
-                <Back/>
+                <Link to='/'><Back/></Link>
                 <div className='event-header-btns-right'>
                     <External/>
                     <Favorite/>
